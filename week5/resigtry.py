9f3fbb8b331a461513771a3b3a1db5ea6eed6eb9
StiveMan1 <04024004zh@gmail.com>
2022-10-12 20:10:42 +0600

    register 1.0

--- week5/resigtry.py
+++ week5/resigtry.py
@@ -1,19 +1,76 @@
+import grpc  
+# import service_pb2
+# import service_pb2_grpc
+from concurrent import futures
+import time 
+from sys import argv
+import random
+
+
+def getFromArgsRegistry(args):
+	return args[1].split(':')[0], int(args[1].split(':')[1]), int(args[2])
+
+ipaddr = ""
+port = 0
+m = 0
+chord_info = {}
+
+def getNonColision():
+	res = int(random.uniform(0, 2**m))
+	while res in chord_info.keys():
+		print(res)
+		res = int(random.uniform(0, 2**m))
+	return res
+
+def findNext(id, main_id):
+	for _id in chord_info.keys():
+		if _id >= id:
+			return _id
+	if len(chord_info.keys()) > 0:
+		return chord_info.keys()[0]
+	return main_id
+
 
 
 def register(ipaddr, port):
-	pass
+	if len(chord_info.keys()) < 2**m:
+		id = getNonColision()
+		chord_info[id] = (ipaddr, port)
+		return (id, m)
+	return (-1, "Chord is full")
+
 
 def deregister(id):
-	pass
+	if not id in chord_info.keys():
+		return (False, f"no {id} in chord")
+	del chord_info[id]
+	return (True, f"successful deregister {id}")
+
 
 def populate_finger_table(id):
-	pass
+	finger_ids = {}
+	for i in random(m):
+		finger_ids[findNext((id + 2 ** i) % (2**m), id)] = 1
+	
+	result_list = []
+	for _id in finger_ids.keys():
+		result_list = (_id, f"{chord_info[_id][0]}:{chord_info[_id][1]}")
+
+	return result_list
+
 
 def get_chord_info():
-	pass
+	result = []
+	for id in chord_info.keys():
+		result.append((id, f"{chord_info[id][0]}:{chord_info[id][1]}"))
+	return result
+
 	
 def main():
-	pass 
+	global ipaddr, port, m
+	ipaddr, port, m = getFromArgsRegistry(argv)
+	print(ipaddr, port, m)
 
 if __name__ == '__main__':
+	random.seed(0)
 	main()
